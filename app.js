dfd905edf566853593d97bd52d2048ca226c3114
GiannisTzonis <tzonis.john@hotmail.com>
2022-12-07 19:53:19 +0200

    added response

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const http = require("http");
 // Create Server Callback
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  // process.exit()
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My Server</title ></head >");
+  res.write("<body><h1>Finally</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
