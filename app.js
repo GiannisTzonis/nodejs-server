f4c20dd510bbbb3f92430df992af95ca6837971f
GiannisTzonis <tzonis.john@hotmail.com>
2022-12-08 17:45:24 +0200

    added form form,input and 2nd page

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const http = require("http");
+const fs = require("fs");
 
-// Create Server Callback
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit()
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
-  res.write("<head><title>My Server</title ></head >");
-  res.write("<body><h1>Finally</h1></body>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
   res.write("</html>");
   res.end();
 });
 
-server.listen(3000);
+server.listen(4000);
